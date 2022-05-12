d49622af04f4223fe96c8a40bb7b9f165a6e3748
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-12 12:27:22 +0700

    [ADD] convert plain html to nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/homepage.css";
+import "../styles/utilities.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
