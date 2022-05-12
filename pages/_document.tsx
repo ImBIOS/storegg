5d391859088bc1a58ebf869eebfb564cb5669adf
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-05-12 19:59:35 +0700

    [MODIFY] change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/icon/logo.svg" />
+
           {/* <!-- Bootstrap CSS --> */}
           <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
